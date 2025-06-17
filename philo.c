a7d5307b4ef470b83ac1c832542270409ade80a6
Abderahime Touaitia <abtouait@c2r7p2.42nice.fr>
2025-06-17 12:15:57 +0200

    l

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: abtouait <abtouait@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 00:06:06 by abtouait          #+#    #+#             */
-/*   Updated: 2025/06/07 03:23:21 by abtouait         ###   ########.fr       */
+/*   Updated: 2025/06/17 11:54:52 by abtouait         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 void one_philo(t_table *data)
 {
 	data->start_time = get_time_in_u();
-	printf("%lu Philosopher 1 has taken a fork\n",
+	printf("%lu     Philosopher 1 has taken a fork\n",
 		(get_time_in_u() - data->start_time));
 	ft_usleep(data->time_to_die);
-	printf("%lu Philosopher 1 died\n", (get_time_in_u() - data->start_time));
+	printf("%lu    Philosopher 1 died\n", (get_time_in_u() - data->start_time));
 }
+
