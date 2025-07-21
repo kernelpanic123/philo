a23a119eaa55da3829cecc60d5703b222fe4b9a9
poker_FACE <abdertouaitia@gmail.com>
2025-07-21 08:47:17 +0200

    monitor gooooooooood ;)

--- philo_2.c
+++ philo_2.c
@@ -6,7 +6,7 @@
 /*   By: abder <abder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 14:04:47 by abder             #+#    #+#             */
-/*   Updated: 2025/07/21 08:20:47 by abder            ###   ########.fr       */
+/*   Updated: 2025/07/21 08:42:33 by abder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,13 @@ int check_death(t_table *atad)
     i = 0;
     while (i < atad->philo_nbr)
     {
-        printf("DEBUG: check_death PHILO %d => last_meal: %lu | now: %lu | diff: %lu\n",
-       atad->philos[i].id,
-       atad->philos[i].last_meal_time,
-       get_time_in_u(),
-       get_time_in_u() - atad->philos[i].last_meal_time);
         if ((get_time_in_u()) - (atad->philos[i].last_meal_time) > atad->time_to_die)
         {
             pthread_mutex_lock(&atad->philos[i].printf_mutex);
-            printf("%lu Philosopher %d died\n", (get_time_in_u() - atad->start_time), atad->philos[i].id);
+            printf("%lu %d died\n", (get_time_in_u() - atad->start_time), atad->philos[i].id);
             pthread_mutex_unlock(&atad->philos[i].printf_mutex);
             pthread_mutex_lock(&atad->mutex_dead);
             atad->dead_flag = 1;
-            printf("DEBUG: DEAD FLAG SET TO 1 BY PHILO %d\n", atad->philos[i].id);
             pthread_mutex_unlock(&atad->mutex_dead);
             return (1);
         }
@@ -47,10 +41,8 @@ void *monitor_routine(void *arg)
     while (!dead_protected(atad))
     {
         check_death(atad);
-        ft_usleep(40);
+        ft_usleep(10);
     }
-    if (check_death(atad))
-        printf("DEBUG MONITOR: Death detected. Exiting loop.\n");
     return NULL;
 }
 
@@ -81,7 +73,6 @@ void *philo_routine(void *arg)
 		ft_usleep(10);
     while (1)
     {
-        printf("DEBUG PHILO %d LOOP (dead_flag = %d)\n", data->id, dead_protected(data->table));
         if (dead_protected(data->table))
             break ;
         philo_thinking(data);
@@ -91,11 +82,6 @@ void *philo_routine(void *arg)
         if (dead_protected(data->table))
             break ;
         philo_sleep(data);
-        if (dead_protected(data->table))
-        {
-            printf("DEBUG PHILO %d: Detected death flag. Exiting routine.\n", data->id);
-            break;
-        }
     }
     return NULL;
 }
--- philo_3.c
+++ philo_3.c
@@ -6,7 +6,7 @@
 /*   By: abder <abder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 20:00:30 by abder             #+#    #+#             */
-/*   Updated: 2025/07/21 08:13:13 by abder            ###   ########.fr       */
+/*   Updated: 2025/07/21 08:46:31 by abder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,17 @@ void philo_eat(t_info *data)
 {
     pthread_mutex_lock(data->r_fork);
     pthread_mutex_lock(data->l_fork);
+    if (dead_protected(data->table))
+    {
+        pthread_mutex_unlock(data->r_fork);
+        pthread_mutex_unlock(data->l_fork);
+        return ;
+    }
     mutex_printf(data);
     pthread_mutex_lock(&data->meal_mutex);
     data->last_meal_time = get_time_in_u();
     pthread_mutex_unlock(&data->meal_mutex);
     ft_usleep(data->table->time_to_eat);
-    printf("DEBUG PHILO %d finished eating after sleep\n", data->id);
     pthread_mutex_unlock(data->r_fork);
     pthread_mutex_unlock(data->l_fork);
     
